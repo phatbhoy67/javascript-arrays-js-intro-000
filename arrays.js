63db1dc78098523fd40d76de04e338646227919e
Chris Scanlon <chris.scanlon72@gmail.com>
2018-01-09 15:11:10 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,5 @@
-var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+var chocolateBars;
+chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(arr, el) {
   return arr[el, ...arr]
