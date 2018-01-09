ed6271b3b3856ab0bb9163add5347ee819388039
Chris Scanlon <chris.scanlon72@gmail.com>
2018-01-09 15:17:18 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,9 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToEndOfArray(array, element) {
-  return [element, ...array]
+  return [element, ...array];
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  return array.unshift(element)
+  return array.unshift(element);
 }
