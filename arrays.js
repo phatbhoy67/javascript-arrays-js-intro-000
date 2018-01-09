4175538a3754d5f288f33d64d40ee63ec12310ee
Chris Scanlon <chris.scanlon72@gmail.com>
2018-01-09 15:18:16 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-function addElementToEndOfArray(array, element) {
+function addElementToBeginningOfArray(array, element) {
   return [element, ...array];
 }
 
