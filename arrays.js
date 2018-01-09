66ad44876aa6cb568c48889c595cffc65c6c9ef5
Chris Scanlon <chris.scanlon72@gmail.com>
2018-01-09 15:39:09 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,33 +1,37 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-function addElementToBeginningOfArray(array, element) {
+function addElementToBeginningOfArray(array, element){
   return [element, ...array];
 }
 
-function destructivelyAddElementToBeginningOfArray(array, element) {
+function destructivelyAddElementToBeginningOfArray(array, element){
   return array.unshift(element);
 }
 
-function addElementToEndOfArray(array, element) {
+function addElementToEndOfArray(array, element){
   return [...array, element];
 }
 
-function destructivelyAddElementToEndOfArray(array, element) {
+function destructivelyAddElementToEndOfArray(array, element){
   return array.push(element);
 }
 
-function accessElementInArray(array, index) {
+function accessElementInArray(array, index){
   return array[index];
 }
 
-function destructivelyRemoveElementFromBeginningOfArray(array) {
+function destructivelyRemoveElementFromBeginningOfArray(array){
   return array.shift();
 }
 
-function removeElementFromBeginningOfArray(array) {
+function removeElementFromBeginningOfArray(array){
   return array.slice(1);
 }
 
 function destructivelyRemoveElementFromEndOfArray(array){
   return array.pop();
+}
+
+function removeElementFromEndOfArray(array){
+  return array.slice(0, array.length - 1);
 }
